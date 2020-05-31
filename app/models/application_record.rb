51f857b3524636087e7b02ce1fa4c2f5983a2536
Nmerey <merey.nurlan@gmail.com>
2020-05-31 15:39:33 +0300

    Add some comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   class << self
+  	#Pagination logic unified
   	def paginate(cursor = 0,limit = 10)
   		self.where("id > ?", cursor).limit(limit)
   	end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe Product, type: :model do
   		expect(Product.paginate).to eq(@products.first(10))
   	end
 
-  	it "def prev should return last ten products" do
+  	it "def prev should return last 10 products" do
   		@products = create_list(:product, 20)
+  		#It is +1 because it selects products from id < last_product
   		expect(Product.prev(@products.last.id + 1)).to eq(@products.last(10)) 
   	end
   end
